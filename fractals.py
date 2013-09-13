5a39cf738b62ed4dc03cfc37fb3f1ebf7b73cf54
Patrick <patrick@patricks-iMac.local>
2013-09-12 17:55:11 -0700

    got rid of comment code

--- fractals.py
+++ fractals.py
@@ -28,23 +28,11 @@ def render(cn):
 	for x in range(-xd/2,xd/2):
 		for y in range(-yd/2,yd/2):
 			temp = complex(normalize(x,xd), normalize(y,yd))
-			c = f(temp,cn,511) #384-1
+			c = f(temp,cn,511) #512-1
 			r = 2 * (c % 64)
 			g = 2 * ((c >> 3)% 64)
 			b = 2 * ((c >> 6)% 64)
 			color = (r,g,b)
-			#if c <= 1:
-			#	color = (128,0,0)
-			#elif c <= 2:
-			#	color = (255,0,0)
-			#elif c <= 4:
-			#	color = (0,128,0)
-			#elif c <= 8:
-			#	color = (0,255,0)
-			#elif c <= 16:
-			#	color = (0,0,128)
-			#elif c <= 32:
-			#	color = (0,0,255)
 			put_pix(x,y,color)
 		pygame.display.update()
 
